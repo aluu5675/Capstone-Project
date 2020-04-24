be9b560b4ca9efdb2c8e25f8cfb802deb557eb54
Jonathan Lin <jonathan.lin1138@live.com>
2020-04-23 23:09:14 -0700

    Fixed load errors

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import { Navbar } from './Navbar.js'
+import { ResourcesPage, ResourceList } from './resourceConnector.js'
 import './style.css';
 
 export default class App extends Component {
   render() { 
     return (
-    <div className="App">
-      <Navbar>
-
-      </Navbar>
-    </div>
+      <ResourcesPage />
     );
   }
 }
--- my-app/src/resourceConnector.js
+++ my-app/src/resourceConnector.js
@@ -4,7 +4,7 @@ import './style.css';
 // resourceConnector.js connects components related to the resources page
 
 // component representing an individual resource item
-export class ResourceItem {
+export class ResourceItem extends React.Component{
     render() {
         let resourceName = this.props.resourceName;
         let resourceDescription = this.props.resourceDescription 
@@ -18,27 +18,39 @@ export class ResourceItem {
 }
 
 // component representing full list view of all rendered resources
-export class ResourceList {
+export class ResourceList extends React.Component{
     render() {
         // takes in an object with fields 'resourceName' and 'resourceDescription' and creates an array of individual resource components  
         let resourceItems = this.props.resources.map((resource) => {
              let resourceComponent = <ResourceItem resourceName = {resource.resourceName} resourceDescription={resource.resourceDescription}/>;
              return resourceComponent;
         })
+        return (resourceItems)
     }
 }
 
-export class ResourcesPage {
+// component representing full resource page
+export class ResourcesPage extends React.Component {
     render() {
-        let 
+        let resources = [
+            {resourceName: "Food Banks", resourceDescription: "List of programs and food banks in Washington to help find or buy food"},
+            {resourceName: "Saving Dollars on Food", resourceDescription: "Tips and tricks for saving money when buying food"}
+        ]
         return (
             <body>
                 <nav>
 
                 </nav>
-                <main>
-                    <h1>Food Resources</h1>
+                <main className="flex-container">
+                    <div className="sidenav">
 
+                    </div>
+                    <div className="content">
+                        <h1>Food Resources</h1>
+                        <ul>
+                            <ResourceList resources={resources} />
+                        </ul>
+                    </div>
                 </main>
             </body>
         )
