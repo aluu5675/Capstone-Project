377a13463eb9849df0da1f8ff29b93c36dfb7947
Jonathan <jonathan.lin1138@live.com>
2020-05-04 22:22:20 -0700

    merging

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Navbar } from './Navbar.js'
-import { ResourcesPage, ResourceList } from './resourceConnector.js'
+import { ResourcesPage, ResourceList } from './ResourcesPage.js'
+import { ResourceCategoriesPage } from './ResourceCategoriesPage.js'
 import './style.css';
 
 export default class App extends Component {
   render() { 
     return (
-      <ResourcesPage />
+      <ResourceCategoriesPage />
     );
   }
 }
